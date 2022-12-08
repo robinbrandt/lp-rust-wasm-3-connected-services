a128809754e6076ae80d1e40195b93459969720e
Michael Yuan <michael@secondstate.io>
2022-12-08 06:59:18 +0000

    Add two services
    Signed-off-by: Michael Yuan <michael@secondstate.io>

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # Add a dependent microservice and UI
 
-Repository for liveProject: Add a dependent microservice and UI
+## Build
+
+```bash
+cd sales_tax_rate
+cargo build --target wasm32-wasi --release
+
+cd order_total
+cargo build --target wasm32-wasi --release
+```
+
+## Run
+
+```bash
+cd sales_tax_rate
+wasmedge target/wasm32-wasi/release/sales_tax_rate_lookup.wasm
+
+cd order_total
+wasmedge --env "SALES_TAX_RATE_SERVICE=http://127.0.0.1:8001/find_rate" target/wasm32-wasi/release/order_total.wasm
+```
+
+## Test
+
+Run the following from another terminal.
+
+```bash
+$ curl http://localhost:8002/compute -X POST -d @order.json
+{
+  "order_id": 123,
+  "product_id": 321,
+  "quantity": 2,
+  "subtotal": 20.0,
+  "shipping_address": "123 Main St, Anytown USA",
+  "shipping_zip": "78701",
+  "total": 21.65
+}
+```
---
+++ order_total/src/main.rs
@@ -0,0 +1,107 @@
+#[macro_use]
+extern crate lazy_static;
+
+use std::net::SocketAddr;
+use std::convert::Infallible;
+use std::str;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Method, Request, Response, StatusCode, Server};
+use serde::{Deserialize, Serialize};
+
+lazy_static! {
+    static ref SALES_TAX_RATE_SERVICE: String = {
+        if let Ok(url) = std::env::var("SALES_TAX_RATE_SERVICE") {
+            url
+        } else {
+            "http://localhost:8001/find_rate".into()
+        }
+    };
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Order {
+    order_id: i32,
+    product_id: i32,
+    quantity: i32,
+    subtotal: f32,
+    shipping_address: String,
+    shipping_zip: String,
+    total: f32,
+}
+
+/*
+impl Order {
+    fn new(
+        order_id: i32,
+        product_id: i32,
+        quantity: i32,
+        subtotal: f32,
+        shipping_address: String,
+        shipping_zip: String,
+        total: f32,
+    ) -> Self {
+        Self {
+            order_id,
+            product_id,
+            quantity,
+            subtotal,
+            shipping_address,
+            shipping_zip,
+            total,
+        }
+    }
+}
+*/
+
+/// This is our service handler. It receives a Request, routes on its
+/// path, and returns a Future of a Response.
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, anyhow::Error> {
+    match (req.method(), req.uri().path()) {
+        // Serve some instructions at /
+        (&Method::GET, "/") => Ok(Response::new(Body::from(
+            "Try POSTing data to /compute such as: `curl localhost:8002/compute -XPOST -d '...'`",
+        ))),
+
+        (&Method::POST, "/compute") => {
+            let byte_stream = hyper::body::to_bytes(req).await?;
+            let mut order: Order = serde_json::from_slice(&byte_stream).unwrap();
+
+            let client = reqwest::Client::new();
+            let rate = client.post(&*SALES_TAX_RATE_SERVICE)
+                .body(order.shipping_zip.clone())
+                .send()
+                .await?
+                .text()
+                .await?
+                .parse::<f32>()?;
+
+            order.total = order.subtotal * (1.0 + rate);
+            Ok(Response::new(Body::from(serde_json::to_string_pretty(&order)?)))
+        }
+
+        // Return the 404 Not Found for other routes.
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8002));
+    let make_svc = make_service_fn(|_| {
+        async move {
+            Ok::<_, Infallible>(service_fn(move |req| {
+                handle_request(req)
+            }))
+        }
+    });
+    let server = Server::bind(&addr).serve(make_svc);
+    dbg!("Server started on port 8002");
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+    Ok(())
+}
